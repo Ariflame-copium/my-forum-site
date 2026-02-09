3aaaf7ef77e3d51f8925366cef83dcf3d1104762
ariflame killaz <sahahevcov1954@gmail.com>
2026-02-09 16:42:48 +0200

    new fix

--- src/components/Post/PostItem.tsx
+++ src/components/Post/PostItem.tsx
@@ -8,15 +8,15 @@ interface PostItemProp {
 export const PostItem: React.FC<PostItemProp> = ({ post }) => {
     return (
         <S.Card>
-            <S.Title>{post.title} </S.Title>
+            <S.Title>{post?.title} </S.Title>
             <S.AuthorInfo>
-                <Link to={`/profile/${post.author.id}`}>{post.author.username}</Link>
-                <span>{post.author.role}</span>
-                <span>{post.createdAt}</span>
+                <Link to={`/profile/${post?.author.id}`}>{post?.author.username}</Link>
+                <span>{post?.author.role}</span>
+                <span>{post?.createdAt}</span>
             </S.AuthorInfo>
             <div style={{ marginTop: '10px' }}>
-                <Link to={`/post/${post.id}`} style={{ color: '#646cff', fontSize: '0.9rem' }}>
-                    Читати далі та коментувати ({post.comments?.length || 0})
+                <Link to={`/post/${post?.id}`} style={{ color: '#646cff', fontSize: '0.9rem' }}>
+                    Читати далі та коментувати ({post?.comments?.length || 0})
                 </Link>
             </div>
         </S.Card>
